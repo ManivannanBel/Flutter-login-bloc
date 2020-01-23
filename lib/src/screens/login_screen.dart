1659afc263879ff4e0c0f7887847541cd168b665
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-01-23 22:51:11 +0530

    rxdart

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -55,11 +55,17 @@ class LoginScreen extends StatelessWidget{
     );
   }
 
-  Widget submitButton(){
-    return RaisedButton(
-      child: Text('Login'),
-      color: Colors.blue,
-      onPressed: (){},
+  Widget submitButton(Bloc bloc){
+
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot){
+        return RaisedButton(
+        child: Text('Login'),
+        color: Colors.blue,
+        onPressed: (){},
+        );  
+      },
     );
   }
 }
